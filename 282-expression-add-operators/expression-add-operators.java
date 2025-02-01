815f62abc75f0d17a7b7894fcea86bd0e1cafcfc
PRAJWALSINKAR <157676503+PRAJWALSINKAR@users.noreply.github.com>
2025-02-01 20:10:43 +0530

    Time: 119 ms (15.92%) | Memory: 46.1 MB (6.60%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.java
+++ 282-expression-add-operators/expression-add-operators.java
@@ -1,83 +1,44 @@
-class Solution {
-public List<String> addOperators(String num, int target) {
+public class Solution {
+    public List<String> addOperators(String num, int target) {
         List<String> res = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        helper(res, sb, num, target, 0, 0, 0);
+        // Start the backtracking process
+        backtrack(res, num, target, 0, 0, 0, "");
         return res;
     }
 
-    public void helper(List<String> res, StringBuilder sb, String num, int target, int index, long prevNum, long currentNum) {
+    private void backtrack(List<String> res, String num, int target, int index, long prevNum, long currentResult, String expression) {
+        // If we have processed all characters
         if (index == num.length()) {
-            if (currentNum == target) {
-                res.add(sb.toString());
+            if (currentResult == target) {
+                res.add(expression);  // Add the expression to result
             }
             return;
         }
-
-        // Loop through the string and add operators
+        
+        // Try every possible cut in the string (i.e., all substrings)
         for (int i = index + 1; i <= num.length(); i++) {
             String currentStr = num.substring(index, i);
             long currValue = Long.parseLong(currentStr);
-
+            
             // Skip numbers that start with '0' but are not "0"
             if (currentStr.length() > 1 && currentStr.charAt(0) == '0') {
                 continue;
             }
 
-            int lenBefore = sb.length();
+            int lenBefore = expression.length();
             if (index == 0) {
-                // No operator before the first number
-                sb.append(currentStr);
-                helper(res, sb, num, target, i, currValue, currValue);
-                sb.delete(lenBefore, sb.length());
+                // First number, no operator before it
+                backtrack(res, num, target, i, currValue, currValue, currentStr);
             } else {
-                // Try the '+' operator
-                sb.append("+").append(currentStr);
-                helper(res, sb, num, target, i, currValue, currentNum + currValue);
-                sb.delete(lenBefore, sb.length());
+                // Try adding '+'
+                backtrack(res, num, target, i, currValue, currentResult + currValue, expression + "+" + currentStr);
 
-                // Try the '-' operator
-                sb.append("-").append(currentStr);
-                helper(res, sb, num, target, i, -currValue, currentNum - currValue);
-                sb.delete(lenBefore, sb.length());
+                // Try adding '-'
+                backtrack(res, num, target, i, -currValue, currentResult - currValue, expression + "-" + currentStr);
 
-                // Try the '*' operator
-                sb.append("*").append(currentStr);
-                helper(res, sb, num, target, i, prevNum * currValue, currentNum - prevNum + prevNum * currValue);
-                sb.delete(lenBefore, sb.length());
+                // Try adding '*'
+                backtrack(res, num, target, i, prevNum * currValue, currentResult - prevNum + prevNum * currValue, expression + "*" + currentStr);
             }
         }
-    }
-    public long calculate(StringBuilder sb) {
-        long result = 0;
-        long currentNum = 0;
-        long prevNum = 0;
-        char operator = '+';
-
-        for (int i = 0; i < sb.length(); i++) {
-            char ch = sb.charAt(i);
-
-            if (Character.isDigit(ch)) {
-                currentNum = currentNum * 10 + (ch - '0');
-            }
-
-            if (i == sb.length() - 1 || ch == '+' || ch == '-' || ch == '*') {
-                if (operator == '+') {
-                    result += currentNum;
-                } else if (operator == '-') {
-                    result -= currentNum;
-                } else if (operator == '*') {
-                    result = result - prevNum + prevNum * currentNum;
-                }
-
-                if (ch == '+' || ch == '-' || ch == '*') {
-                    operator = ch;
-                }
-
-                prevNum = currentNum;
-                currentNum = 0;
-            }
-        }
-        return result;
     }
 }
