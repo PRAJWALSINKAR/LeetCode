5f64b1ffb3cb91919cf8c4f0947b0bfa6a200ae4
PRAJWALSINKAR <157676503+PRAJWALSINKAR@users.noreply.github.com>
2025-02-17 21:38:06 +0530

    Time: 1 ms (99.76%) | Memory: 47.2 MB (92.83%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,33 +1,28 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int m = grid.length;
+        int m  = grid.length;
         int n = grid[0].length;
-        int[][] dp = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            Arrays.fill(dp[i], -1);
+
+        int [][] dp = new int[m][n];
+        for(int i=0 ; i< m ; i++){
+            Arrays.fill(dp[i] , -1);
         }
-        return helper(m - 1, n - 1, dp, grid);
+        return f(m-1 , n-1 , dp , grid);
     }
-
-    public int helper(int row, int col, int[][] dp, int[][] arr) {
-        if (row == 0 && col == 0) {
-            return arr[row][col];
+    public static int f(int row , int col , int [][] dp , int [][] arr){
+        if(row == 0 && col == 0 ){
+            return arr[0][0];
         }
-
-        if (dp[row][col] != -1) return dp[row][col];
-
+        if(dp[row][col] != -1) return dp[row][col];
         int up = Integer.MAX_VALUE;
         int left = Integer.MAX_VALUE;
 
-        if (row > 0) {
-            up = helper(row - 1, col, dp, arr);
+        if(col > 0){
+            left = f(row , col-1 , dp , arr);
         }
-        if (col > 0) {
-            left = helper(row, col - 1, dp, arr);
+         if(row > 0){
+            up = f(row-1 , col , dp , arr);
         }
-
-        dp[row][col] = arr[row][col] + Math.min(up, left);
-
-        return dp[row][col];
+        return dp[row][col] = arr[row][col] + Math.min(left , up) ;
     }
 }
