9c9291d9549a12979dce6d7e6cd4de02c312f35c
PRAJWALSINKAR <157676503+PRAJWALSINKAR@users.noreply.github.com>
2025-03-01 23:02:01 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (10.63%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -15,10 +15,10 @@ class Solution {
         int left = 0;
         int up = 0;
         if(col > 0){
-        left = f(row-1 , col , dp);
+        left = f(row , col-1 , dp);
         }
         if(row > 0){
-        up = f(row , col-1 , dp );
+        up = f(row-1 , col , dp );
          }
        return dp[row][col] = left + up;  
         }
