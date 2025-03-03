373f8e2356611bcb0c8cd00d0fa8afcbe52cb9db
PRAJWALSINKAR <157676503+PRAJWALSINKAR@users.noreply.github.com>
2025-03-03 20:51:14 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (28.04%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,31 +1,46 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length; 
-           int [][] dp = new int[m][n];
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {  
+        int n = obstacleGrid.length;
+        int m = obstacleGrid[0].length;
+
+       int prev[] = new int[m];
+        
+        for (int i = 0; i < n; i++) {
+            // Create a temporary array for the current row
+            int temp[] = new int[m];
             
-           for(int i = 0 ; i< m ; i++){
-              for(int j = 0 ;j< n;j++){
-                 if(obstacleGrid[i][j] == 1 ){
-                 dp[i][j] = 0;
-                 continue;
-                }
-                 if(i==0 && j==0){
-                    dp[i][j] = 1;
+            for (int j = 0; j < m; j++) {
+                // Base conditions
+                if (obstacleGrid[i][j] == 1) {
+                    temp[j] = 0; // If there's an obstacle, no paths can go through here.
                     continue;
                 }
+                if (i == 0 && j == 0) {
+                    temp[j] = 1; // There's one valid path to the start cell.
+                    continue;
+                }
+
                 int up = 0;
-                int down = 0; 
-              if( j >0){
-             up = dp[i][j-1] ;
-              }
-              if(i > 0){
-             down = dp[i-1][j];
-              }
-              dp[i][j]= up + down;
+                int left = 0;
+
+                // Check if moving up is possible
+                if (i > 0)
+                    up = prev[j];
+                
+                // Check if moving left is possible
+                if (j > 0)
+                    left = temp[j - 1];
+
+                // Calculate the number of paths by adding paths from above and from the left
+                temp[j] = up + left;
             }
-           }
-           return dp[m-1][n-1];
+            
+            // Update the previous row with the values of the current row
+            prev = temp;
+        }
+
+        // The final result is stored in the last element of the previous row
+        return prev[m - 1];
     }
 }
   
