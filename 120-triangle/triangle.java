5f8f329e9566f615909ca89d3886be868b3275f9
PRAJWALSINKAR <157676503+PRAJWALSINKAR@users.noreply.github.com>
2025-03-03 23:59:00 +0530

    Time: 4 ms (48.34%) | Memory: 44 MB (76.20%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,28 +1,21 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int m = triangle.size();
-        int[][] dp = new int[m][m]; // Using m since it's a triangle
-        
-        for (int i = 0; i < m; i++) {
-            Arrays.fill(dp[i], -1);
+        int n = triangle.size();
+        int [][] dp = new int[n][n];
+
+        for(int j = 0;j < n;j++){     // step 1 :- base condition (last row same)
+            dp[n-1][j] = triangle.get(n-1).get(j); 
         }
+       // step 2 :- we find out every point minimum value
+        for(int i = n-2; i >= 0 ; i--){   // last 2nd row -> last col -> TO-> last 2nd row -> 0 col
+            for(int j = i ; j>=0 ; j-- ){   // last 2nd row(ex 3 row -> the elemeant in that is also 3 )
+                int down = triangle.get(i).get(j) + dp[i+1][j]; 
+                int downleft = triangle.get(i).get(j) + dp[i+1][j+1];
+               
+               dp[i][j] = Math.min(down , downleft); 
 
-        return f(0, 0, dp, triangle); // Start from the top of the triangle
-    }
-
-    public static int f(int row, int col, int[][] dp, List<List<Integer>> triangle) {
-        int m = triangle.size();
-
-        // Base case: last row
-        if (row == m - 1) {
-            return triangle.get(row).get(col);
+            }
         }
-        if (dp[row][col] != -1) return dp[row][col];
-
-        // Move down and diagonally down-left
-        int down = f(row + 1, col, dp, triangle);
-        int downLeft = f(row + 1, col + 1, dp, triangle);
-
-        return dp[row][col] = triangle.get(row).get(col) + Math.min(down, downLeft);
+        return dp[0][0];
     }
 }
