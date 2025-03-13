f114076d18bb89a05e00381cd36142991366e7c9
PRAJWALSINKAR <157676503+PRAJWALSINKAR@users.noreply.github.com>
2025-03-13 17:41:25 +0530

    Time: 3 ms (100.00%) | Memory: 128.9 MB (20.11%) - LeetSync

---
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minZeroArray(int[] nums, int[][] queries) {
+        int n = nums.length, sum = 0, k = 0;
+        int[] cnt = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            while (sum + cnt[i] < nums[i]) {
+                if (k == queries.length) return -1;
+
+                int l = queries[k][0];
+                int r = queries[k][1];
+                int val = queries[k][2];
+                k++;
+
+                if (r < i) continue;
+                cnt[Math.max(l, i)] += val;
+                if (r + 1 < n) {
+                    cnt[r + 1] -= val;
+                }
+            }
+            sum += cnt[i];
+        }
+        return k;
+    }
+}
