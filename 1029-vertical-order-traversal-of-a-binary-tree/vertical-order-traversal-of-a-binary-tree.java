d6228349934b756b38d503843748b5b8e0e0c9d3
PRAJWALSINKAR <157676503+PRAJWALSINKAR@users.noreply.github.com>
2025-04-07 11:10:01 +0530

    Time: 4 ms (42.07%) | Memory: 42.9 MB (23.15%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -15,50 +15,40 @@
  */
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-      TreeMap< Integer , TreeMap<Integer,PriorityQueue<Integer>>> nodes = new TreeMap<>();
-      //for sorting (+1 ,0 ,-1) we use TreeMap
-      //for each level sorting -> priorityQ (level-3 -> [15 , 3] => [3 , 15])
+        TreeMap<Integer,TreeMap<Integer , PriorityQueue<Integer>>> nodes = new TreeMap<>();
 
-    Queue<Pair<TreeNode,Pair<Integer ,Integer>>> q = new LinkedList<>();
-    //store info of each node (node , (vertical , level))
-    
-    q.add(new Pair<>(root , new Pair<>(0 , 0)));
-    //store a first root node (Node, pair<vertical , level>)
-    
-    while(!q.isEmpty()){
-        Pair<TreeNode , Pair<Integer , Integer >> curr = q.poll();//remove top element
-
-        TreeNode node = curr.getKey();
-        int vertical = curr.getValue().getKey();//node -> values(key)
-        int level = curr.getValue().getValue(); // Node -> value(value)
-
-//→ If this vertical line (column) is not already present → create new TreeMap (for levels)  
-        nodes.putIfAbsent(vertical, new TreeMap<>());
-//→ If this level is not present inside vertical line → create new PriorityQueue
-        nodes.get(vertical).putIfAbsent(level, new PriorityQueue<>());
-//Finally add the node value into the PriorityQueue.
-        nodes.get(vertical).get(level).add(node.val);
-
-        if(node.left != null){
-           q.add(new Pair<>(node.left , new Pair<>(vertical -1 , level + 1)));
-        }
-
+      Queue<Pair<TreeNode,Pair<Integer , Integer>>> q = new LinkedList<>();
         
-        if(node.right != null){
-           q.add(new Pair<>(node.right , new Pair<>(vertical +1 , level + 1)));
-        }
-    }
-        List<List<Integer>> ans = new ArrayList<>();
+        q.add(new Pair<>(root ,  new Pair<>(0,0)));
 
-        for(TreeMap<Integer,PriorityQueue<Integer>> verticalMap : nodes.values()){
-            List<Integer> col = new ArrayList<>();
-            for(PriorityQueue<Integer> pq : verticalMap.values()){
-                while(!pq.isEmpty()){
-                    col.add(pq.poll());
-                }
-            }
-            ans.add(col);   
+        while(!q.isEmpty()){
+        Pair<TreeNode,Pair<Integer , Integer>> curr = q.poll();
+         TreeNode nod = curr.getKey();
+         int vertical =  curr.getValue().getKey();
+         int level = curr.getValue().getValue();
+
+        nodes.putIfAbsent(vertical , new TreeMap<>());
+        nodes.get(vertical).putIfAbsent(level, new PriorityQueue<>());
+        nodes.get(vertical).get(level).add(nod.val);
+
+        if(nod.left != null){
+            q.add(new Pair<>(nod.left , new Pair<>(vertical -1 , level +1)));
         }
-        return ans;
+        
+        if(nod.right != null){
+            q.add(new Pair<>(nod.right , new Pair<>(vertical + 1 , level +1)));
+        }
+        }
+       List<List<Integer>> ans = new ArrayList<>();
+       for(TreeMap<Integer ,PriorityQueue<Integer>> map : nodes.values()){
+        List<Integer> lis = new ArrayList<>();
+        for(PriorityQueue<Integer> pq : map.values()){
+            while(!pq.isEmpty()){
+               lis.add(pq.poll());
+            }
+        }
+        ans.add(lis);
+       }
+       return ans;
     }
 }
