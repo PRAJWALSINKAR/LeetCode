807a7957326a272df356f47c138d81af5be93a47
PRAJWALSINKAR <157676503+PRAJWALSINKAR@users.noreply.github.com>
2025-04-16 18:11:31 +0530

    Time: 66 ms (66.96%) | Memory: 57.8 MB (28.40%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,38 +1,34 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int n = heights.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
         Stack<Integer> stk = new Stack<>();
-
-        for (int i = 0; i < n; i++) {
-            while (!stk.isEmpty() && heights[stk.peek()] >= heights[i]) {
-                stk.pop();
-            }
-            if (stk.isEmpty()) {
-                left[i] = -1;
-            } else {
-                left[i] = stk.peek();
-            }
-            stk.push(i);
-        }
-        stk.clear();
-        for (int i = n-1; i >= 0; i--) {
-            while (!stk.isEmpty() && heights[stk.peek()] > heights[i]) {
-                stk.pop();
-            }
-            if (stk.isEmpty()) {
-                right[i] = n;
-            } else {
-                right[i] = stk.peek();
-            }
-            stk.push(i);
-        }
+        int n = heights.length;
+        int element = 0;
         int max = 0;
-        for(int i = 0; i< n; i++){
-            int width = right[i] - left[i]-1;
-            int area = width  * heights[i];
-            max = Math.max(max , area);
+        for (int i = 0; i < n; i++) {
+            int next = n;
+            int prev = -1;
+            while (!stk.isEmpty() && heights[i] < heights[stk.peek()]) {
+                next = i;
+                element = heights[stk.pop()];
+                if (!stk.isEmpty()) {
+                    prev = stk.peek();
+                } else {
+                    prev = -1;
+                }
+                max = Math.max(max, element * (next - prev - 1));
+            }
+            stk.push(i);
+        }
+        while (!stk.isEmpty()) {
+            int next = n;
+            int prev = -1;
+            element = heights[stk.pop()];
+            if (!stk.isEmpty()) {
+                prev = stk.peek();
+            } else {
+                prev = -1;
+            }
+            max = Math.max(max, element * (next - prev - 1));
         }
         return max;
     }
