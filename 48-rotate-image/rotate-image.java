4b9e347c0969808141c9e6fb345cd56e8f5b403f
PRAJWALSINKAR <157676503+PRAJWALSINKAR@users.noreply.github.com>
2025-05-29 23:28:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (14.90%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -2,6 +2,8 @@ class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
         
+        // step 1 : firt we swap like(1,2) to (2 , 1)
+        // step 2 : just reverse the every rows
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 int temp = matrix[i][j];
@@ -21,3 +23,9 @@ class Solution {
         }
     }
 }
+/*
+Time comlixity :- O(n²)  - each element once
+
+Space complixity:- O(1)
+
+*/
