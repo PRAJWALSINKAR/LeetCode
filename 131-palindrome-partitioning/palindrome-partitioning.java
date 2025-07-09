3918bf1235af8305260c4e9b40821610ba920ba6
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-07-10 00:39:14 +0530

    Time: 8 ms (53.21%) | Memory: 57.1 MB (44.54%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -2,30 +2,31 @@ class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> res = new ArrayList<>();
         List<String> temp = new ArrayList<>();
-        backtrack(0,s,res , temp);
+        helper(0, s, res, temp);
         return res;
     }
-    public void backtrack(int ind , String s , List<List<String>> res , List<String> temp){
-        if(ind  == s.length()){
+
+    public void helper(int idx , String s , List<List<String>> res , List<String> temp){
+        if(idx == s.length()){
             res.add(new ArrayList<>(temp));
             return;
         }
         
-        for(int i = ind ; i<s.length();i++){
-             if(ispalindrom(s,ind , i)){
-            temp.add(s.substring(ind , i+1));// aabc
-            backtrack(i +1,s,res , temp);
-            temp.remove(temp.size()-1);
-            }    
+        for(int i = idx ; i< s.length() ; i++){
+        if(isPalindrom(s , idx , i)){
+            temp.add(s.substring(idx ,i+1 ));
+            helper(i+1 , s , res , temp);  //imp
+            temp.remove(temp.size() -1 );
         }
-
     }
-    public boolean ispalindrom(String s  , int start  , int end){
-        while(start<end){
-            if(s.charAt(start++) != s.charAt(end--)){
+}
+
+    public boolean isPalindrom(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start++) != s.charAt(end--)) {
                 return false;
             }
         }
-         return true;
+        return true;
     }
 }
