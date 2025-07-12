6a75b3c1a77c8ba1d3f91fc1145fbeadc38a6582
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-07-13 01:35:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (82.36%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,16 +1,19 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         int n = nums.length;
-        List<List<Integer>> ans = new ArrayList<>();
-        for(int i =0; i< (1<< n) ; i++){
-            List<Integer> ad = new ArrayList<>();
-            for(int j = 0 ;j<n ;j++){
-                if((i & (1 << j)) !=  0 ){
-                    ad.add(nums[j]);
+        List<List<Integer>> res = new ArrayList<>();
+
+        for(int i = 0; i < (1 << n) ; i++){ // first it run to 2*n  values
+            List<Integer> temp = new ArrayList<>();
+
+            for(int j = 0 ; j< n ;j++){// cheak every bit pos wheter it 1 or 0
+
+                if(((1 << j)& i) != 0){ // Checks whether the jth bit in i is 1
+                    temp.add(nums[j]);
                 }
             }
-            ans.add(ad);
+            res.add(temp);
         }
-        return ans;
+        return res;
     }
 }
