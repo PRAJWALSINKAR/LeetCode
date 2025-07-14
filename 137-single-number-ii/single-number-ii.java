3a95c3ffd062f62cc121e101c976b07f9d261793
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-07-14 21:03:21 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (90.25%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,68 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ones = 0;
-        int twos = 0;
-
-        for(int i =0 ;i<nums.length ; i++){
-            ones = (nums[i] ^ ones) & (~twos);  //it's like condtion =>(a) and (b) is true Then save
-            twos = (nums[i] ^ twos) & (~ones);
+        int ones  = 0; int twos =0;
+        for(int num : nums){
+            ones = (num ^ ones) & ~twos;
+            twos = (num ^ twos) & ~ones;
         }
         return ones;
     }
 }
-
-/* Reffer below approch also 
-
-1) Approach 1: Brute Force
-
-class Solution {
-    public int singleNumber(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for (int x : nums) {
-            map.put(x, map.getOrDefault(x, 0) + 1);
-        }
-
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() == 1) {
-                return entry.getKey();
-            }
-        }
-        
-        return -1;
-    }
-}
---> Time Complexity: O(n)
-One pass to build the map: O(n)
-One pass to find the unique number: O(n) in worst case (all unique keys)
-
---> Space Complexity: O(n)
-In worst case, all n numbers are unique → the map stores all n elements.
----------------------------------------------------------------------------------
-
-2) Approach 2: Bit Manipulation
-
-class Solution {
-  public int singleNumber(int[] nums) {
-    int ans = 0;
-
-    for (int i = 0; i < 32; ++i) {
-      int sum = 0;
-      for (final int num : nums)
-        sum += num >> i & 1;
-      sum %= 3;
-      ans |= sum << i;
-    }
-
-    return ans;
-  }
-}
- 
--->Time Complexity: O(32 * n) = O(n)
-32 iterations (for each bit) × n elements = O(n)
-
---> Space Complexity: O(1)
-Uses only a few integer variables — no extra space that grows with input size.
-
-
-*/
