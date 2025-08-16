cf6ed71fc7a070a7007bd4312a3b6a03f27b3607
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-08-16 08:57:05 +0530

    Time: 1 ms (100.00%) | Memory: 42.2 MB (86.65%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,45 +1,45 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        if (grid == null || grid.length == 0)
-            return 0;
-        int row = grid.length;
-        int col = grid[0].length;
-
-        int[][] time = new int[row][col];
-
-        for (int i = 0; i < row; i++) {
-            Arrays.fill(time[i], Integer.MAX_VALUE);
+        if (grid == null || grid[0].length == 0)
+            return -1;
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] time = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                time[i][j] = Integer.MAX_VALUE;
+            }
         }
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 2) {
-                    dfs(grid, time, i, j, 0);
+                    dfs(i, j, 0, grid, time);
                 }
             }
         }
-
-        int timeRequire = 0;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col ; j++) {
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 1) {
-                    if (time[i][j] == Integer.MAX_VALUE)
+                    if (time[i][j] == Integer.MAX_VALUE) {
                         return -1;
-                    timeRequire = Math.max(timeRequire, time[i][j]);
+                    }
+                    max = Math.max(max, time[i][j]);
                 }
             }
         }
-        return timeRequire;
+        return max;
     }
 
-    public void dfs(int[][] grid, int[][] time, int i, int j, int currTime) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0 || currTime >= time[i][j])
+    public void dfs(int i, int j, int currTime, int[][] grid, int[][] time) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0 || currTime >= time[i][j])
             return;
 
         time[i][j] = currTime;
 
-        dfs(grid, time, i - 1, j, currTime + 1);
-        dfs(grid, time, i + 1, j, currTime + 1);
-        dfs(grid, time, i, j - 1, currTime + 1);
-        dfs(grid, time, i, j + 1, currTime + 1);
+        dfs(i - 1, j, currTime + 1, grid, time);
+        dfs(i + 1, j, currTime + 1, grid, time);
+        dfs(i, j - 1, currTime + 1, grid, time);
+        dfs(i, j + 1, currTime + 1, grid, time);
     }
 }
