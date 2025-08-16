a0cf081797913669f2cc7beeafd3d957a402b662
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-08-16 21:45:10 +0530

    Time: 3 ms (11.67%) | Memory: 42.2 MB (86.65%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,44 +1,42 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int rowLen = grid.length;
-        int colLen = grid[0].length;
-        Queue<Pair<Integer,Integer>> que = new LinkedList<>();
-        int freshOranges = 0;
-    
-        for(int i =0;i<rowLen;i++){
-            for(int j = 0; j< colLen;j++){
-
-                if(grid[i][j] == 2){
-                    que.add(new Pair<>(i,j));
+        int row = grid.length;
+        int col = grid[0].length;
+        int freshOrange = 0;
+        Queue<Pair<Integer, Integer>> que = new LinkedList<>();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 2) {
+                    que.add(new Pair<>(i, j));
                 }
-                else if(grid[i][j] == 1 ) freshOranges++;
+                if (grid[i][j] == 1)
+                    freshOrange++;
             }
         }
+        int min = 0;
+        int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
 
-        int minutes= 0;
-        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
-        while(!que.isEmpty() && freshOranges >0){
+        while (!que.isEmpty() && freshOrange > 0) {
             int size = que.size();
-            minutes++;
+            min++;
 
-            for(int i = 0;i<size;i++){
-                int r = que.peek().getKey();
-                int c = que.peek().getValue();
-                que.remove();
+            for (int i = 0; i < size; i++) {
+             int r = que.peek().getKey();
+             int c = que.peek().getValue();
+             que.remove();
 
-                for(int[] direction : directions){
-                    int cr = r + direction[0];
-                    int cc = c + direction[1];
+                for (int[] diraction : dir) {
+                  int rr = r + diraction[0];
+                  int cc = c + diraction[1];
 
-                    if(cc >= 0 && cc < colLen && cr >= 0 && cr<rowLen && grid[cr][cc] == 1){
-                        grid[cr][cc] = 2;
-                        freshOranges --;
-                        que.add(new Pair<>(cr,cc));
-                    }
+                  if(cc >= 0 && cc <col && rr >= 0 && rr < row && grid[rr][cc] ==1){
+                    grid[rr][cc] = 2;
+                    freshOrange--;
+                    que.add(new Pair<>(rr,cc));
+                  } 
                 }
             }
         }
-        return freshOranges ==0 ? minutes:-1;
+        return freshOrange == 0 ? min :-1;
     }
 }
