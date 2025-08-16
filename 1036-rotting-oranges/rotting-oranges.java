51c664d6ed59fc9e84b7629f57247e8aaa49901a
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-08-16 09:41:40 +0530

    Time: 2 ms (66.70%) | Memory: 42.3 MB (75.49%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,45 +1,59 @@
 class Solution {
+    
     public int orangesRotting(int[][] grid) {
-        if (grid == null || grid[0].length == 0)
-            return -1;
-        int n = grid.length;
-        int m = grid[0].length;
-        int[][] time = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                time[i][j] = Integer.MAX_VALUE;
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
-                    dfs(i, j, 0, grid, time);
+        // First count Fresh Oranges AND Keep rotten oranges in Queue
+        //Store Rotten oranges [row, col ] 
+        Queue<int[]> q = new LinkedList();
+        int row = grid.length;
+        int col = grid[0].length;
+        int minutes = -1;
+        int fresh = 0;
+
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                // Rotten Oranges check
+                if (grid[r][c] == 2) {
+                    q.offer(new int[] { r, c });
+
+                 }  //Fresh Oranges check
+                else if (grid[r][c] == 1) {
+                    fresh++;
                 }
             }
         }
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1) {
-                    if (time[i][j] == Integer.MAX_VALUE) {
-                        return -1;
+        // If no fresh orange is there
+        if(fresh==0) return 0;
+        
+// All 4 directions L R U D
+        int[][] dir={{1,0},{0,1},{-1,0},{0,-1}};
+        while(!q.isEmpty()){
+            minutes++;
+            // How many rotten oranges are there; if we don't store the size BFS will traverse the newly rotten oranges which should rotten in the next minutes
+            int size=q.size();
+
+            while(size-->0){
+                int[] oranges=q.poll();
+                // Check all its neighbor
+                for(int[] d: dir){
+                    // newly rotten oranges row position
+                    int nr=oranges[0]+d[0];
+                    // newly rotten oranges column position
+                    int nc=oranges[1]+d[1];
+// check out of bounds AND if there any fresh oranges are there [in neighbors]
+                    if(nr<row && nc<col && nr>=0 && nc>=0 && grid[nr][nc]==1){
+                        // rotten fresh
+                        grid[nr][nc]=2;
+                        fresh--;
+                        // Adding newly rotten oranges
+                        q.offer(new int[]{nr,nc});
                     }
-                    max = Math.max(max, time[i][j]);
                 }
             }
+
+
         }
-        return max;
-    }
+        // If fresh!=0 means all oranges are not rotten. Hence return -1
+return    fresh==0?minutes:-1;
 
-    public void dfs(int i, int j, int currTime, int[][] grid, int[][] time) {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0 || currTime >= time[i][j])
-            return;
-
-        time[i][j] = currTime;
-
-        dfs(i - 1, j, currTime + 1, grid, time);
-        dfs(i + 1, j, currTime + 1, grid, time);
-        dfs(i, j - 1, currTime + 1, grid, time);
-        dfs(i, j + 1, currTime + 1, grid, time);
     }
 }
