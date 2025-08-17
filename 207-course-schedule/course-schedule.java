a5fee6bcfb328057f1d7514663c1aa8fe7a3d8a0
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-08-17 17:58:27 +0530

    Time: 6 ms (72.76%) | Memory: 45.2 MB (76.20%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,40 +1,34 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj = new ArrayList<>();
-        int[] inDegree = new int[numCourses];
-
-        for (int i = 0; i < numCourses; i++) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i =0 ;i < numCourses ;i++){
             adj.add(new ArrayList<>());
         }
-
-        for (int i = 0; i < prerequisites.length; i++) {
-            int course = prerequisites[i][0];
-            int pre = prerequisites[i][1];
-
-            adj.get(pre).add(course);
-            inDegree[course]++;
+        int [] inDegree = new int[numCourses];
+        for(int arr[] : prerequisites) {
+            adj.get(arr[0]).add(arr[1]);
+            inDegree[arr[1]]++;
         }
-
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++) {
-            if (inDegree[i] == 0) {
-                q.offer(i);
+        Queue<Integer> q =new LinkedList<>();
+        for(int i=0 ; i< numCourses ; i++){
+            if(inDegree[i] == 0){
+                q.add(i);
+                ans.add(i);
             }
         }
+        while(!q.isEmpty()){
+            int node = q.poll();
 
-        int count = 0;
-        while (!q.isEmpty()) {
-            int curr = q.poll();
-            count++;
-
-            for (int neighbor : adj.get(curr)) {
-                inDegree[neighbor]--;
-                if (inDegree[neighbor] == 0) {
-                    q.offer(neighbor);
+            for(int j : adj.get(node)){
+                inDegree[j]--;
+                if(inDegree[j] == 0){
+                    q.add(j);
+                    ans.add(j);
                 }
             }
         }
-        return count == numCourses;
-
+    if(numCourses == ans.size())return true;
+    return false;
     }
 }
