30b4b33d7b4d1c04e3281586139f2931d1e92dc4
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-08-21 17:02:31 +0530

    Time: 15 ms (70.84%) | Memory: 45.6 MB (52.83%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,50 +1,50 @@
-class Solution {
-    public int shortestPathBinaryMatrix(int[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-        Queue<Pair> que = new LinkedList<>();
-        boolean[][] vis = new boolean[rows][cols];
-        int [][] dir = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
-        if(grid[0][0]  != 0)return -1;
-        que.add(new Pair(0,0));
-        int level = 0;
-        while(!que.isEmpty()){
-            ++level;
-            int size = que.size();
+class Pair {
+    int x = 0;
+    int y = 0;
 
-            for(int o = 0;o< size;o++){
-                Pair cur = que.poll();
-                int i = cur.x;
-                int j = cur.y;
-                vis[i][j] = true;
-
-                for(int [] temp : dir){
-                    int row = i+temp[0];
-                    int col = j+temp[1];
-                    if(row >= 0 && col >= 0 && row < rows && col < cols && vis[row][col] == false && grid[row][col] == 0){
-                        que.add(new Pair(row,col));
-                        vis[row][col] = true;
-                        if(vis[rows-1][cols-1] == true){
-                            return level+1;
-                        }
-                    }
-                }  
-            }
-
-        }
-
-        if(vis[rows-1][cols-1]== false) return -1;
-        return level;
+    Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
 
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int rows = grid.length;
+        int cols =grid[0].length;
+        Queue<Pair> q = new LinkedList<>();
+        if(grid[0][0] == 1)return -1;
+        int [][] dir = {{-1,-1} , {-1,0}, {0,-1}, {1,1} ,{1,0} ,{0,1}, {-1,1}, {1,-1}};
 
-class Pair{
-    int x;
-    int y;
-    
-    Pair(int x,int y){
-        this.x = x;
-        this.y= y;
+        q.add(new Pair(0 , 0));
+        boolean[][] vis = new boolean[rows][cols];
+        int level =0;
+        q.add(new Pair(0,0));
+
+       while(!q.isEmpty()){
+        ++level;
+        int size = q.size();
+
+        for(int i =0 ;i<size;i++){
+            Pair curr = q.poll();
+            int rowx = curr.x;
+            int coly = curr.y;
+            vis[rowx][coly] = true;
+        
+            for(int [] temp : dir){
+                int row= temp[0] + rowx;
+                int col = temp[1] + coly;
+                if(row>= 0 && col >=0 && row < rows && col < cols && grid[row][col] == 0 ){
+                    if(!vis[row][col]){
+                        q.add(new Pair(row , col));
+                        vis[row][col] = true;
+                        if(vis[rows-1][cols-1] == true)return level+1;
+                    }
+                }
+            }
+        }
+       }
+       if(vis[rows-1][cols-1] == false)return -1;
+       return level;
     }
 }
