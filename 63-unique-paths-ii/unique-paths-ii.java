656f744621fae10b47ce403634bff14246590ab5
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-08-22 16:11:43 +0530

    Time: 4 ms (5.45%) | Memory: 41.9 MB (37.93%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,46 +1,25 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {  
-        int n = obstacleGrid.length;
-        int m = obstacleGrid[0].length;
+    public int uniquePathsWithObstacles(int[][] arr) {
+    int m = arr.length;
+    int n = arr[0].length;
+    int [][] dp = new int[m][n]; 
+    if(arr[m-1][n-1] == 1)return 0;
+    return helper(0 , 0 , dp , arr);
+   }
+   public static int helper(int row , int col , int[][] dp, int [][] arr){//0 0 
+   
+    if(row >= arr.length  || col >= arr[0].length)return 0;
+    if(arr[row][col] == 1)return 0;
 
-       int prev[] = new int[m];
-        
-        for (int i = 0; i < n; i++) {
-            // Create a temporary array for the current row
-            int temp[] = new int[m];
-            
-            for (int j = 0; j < m; j++) {
-                // Base conditions
-                if (obstacleGrid[i][j] == 1) {
-                    temp[j] = 0; // If there's an obstacle, no paths can go through here.
-                    continue;
-                }
-                if (i == 0 && j == 0) {
-                    temp[j] = 1; // There's one valid path to the start cell.
-                    continue;
-                }
+    if(row == arr.length -1 && col == arr[0].length-1)return 1;
 
-                int up = 0;
-                int left = 0;
 
-                // Check if moving up is possible
-                if (i > 0)
-                    up = prev[j];
-                
-                // Check if moving left is possible
-                if (j > 0)
-                    left = temp[j - 1];
+    if(dp[row][col] != 0)return dp[row][col];
 
-                // Calculate the number of paths by adding paths from above and from the left
-                temp[j] = up + left;
-            }
-            
-            // Update the previous row with the values of the current row
-            prev = temp;
-        }
+    int right = helper(row ,col+1, dp , arr); //0 1  return 1
+    int down = helper(row+1 , col , dp , arr); //1 0 
 
-        // The final result is stored in the last element of the previous row
-        return prev[m - 1];
-    }
+    dp[row][col] = right + down;
+    return dp[row][col];
+   }
 }
-  
