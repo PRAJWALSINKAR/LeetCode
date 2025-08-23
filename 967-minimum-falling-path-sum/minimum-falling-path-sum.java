b80c75b38faae26195cf842f8df352ebf3dc22c9
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-08-23 09:58:25 +0530

    Time: 1 ms (99.84%) | Memory: 45.7 MB (19.39%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,37 +1,33 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
+        int ans = Integer.MAX_VALUE;
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-        int[] prev = new int[m];
-        int[] cur = new int[m];
-
-        // Initialize the first row
-        for (int j = 0; j < m; j++) {
-            prev[j] = matrix[0][j];
-        }
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int up = matrix[i][j] + prev[j];
-
-            int leftDiagonal = (j - 1 >= 0) ? matrix[i][j] + prev[j - 1] : matrix[i][j] + Integer.MAX_VALUE -1000;
-            int rightDiagonal = (j + 1 < m) ? matrix[i][j] + prev[j + 1] : matrix[i][j] + Integer.MAX_VALUE- 1000;
-
-            
-
-                // Store the minimum of the three paths in cur
-                cur[j] = Math.min(up, Math.min(leftDiagonal, rightDiagonal));
+        int[][]dp = new int[m][n];
+        for(int [] arr : dp){
+            Arrays.fill(arr , Integer.MAX_VALUE);
+        } 
+        for(int i =0 ; i<1;i++){
+            for(int j =0 ;j<n ;j++){
+                int val = helper(i , j , matrix , dp);
+                ans= Math.min(ans , val);
             }
-            // Update prev for the next row
-            prev = cur.clone();
         }
+        return ans;
+    }
+    public static int helper(int i , int j , int[][]arr , int [][] dp){
+        if(i < 0 || j <0 || i>= arr.length || j >= arr[0].length)return Integer.MAX_VALUE;
 
-        int mini = Integer.MAX_VALUE;
-        for (int j = 0; j < m; j++) {
-            mini = Math.min(mini, prev[j]);
-        }
+        if(i == arr.length-1)return arr[i][j];
 
-        return mini;
+        if(dp[i][j] != Integer.MAX_VALUE)return dp[i][j];
+
+        int down = helper(i+1 , j , arr , dp);
+        int downLeft = helper(i+1 , j-1 , arr , dp);
+        int downRight = helper(i+1 , j+1 , arr , dp);
+
+        dp[i][j] = arr[i][j] + Math.min(down , Math.min(downLeft ,downRight ));
+        return dp[i][j];
     }
 }
