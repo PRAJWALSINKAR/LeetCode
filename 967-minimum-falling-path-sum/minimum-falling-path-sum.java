99c1c231249a699fd3db17718e4034b6a0c75e44
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-08-23 10:44:36 +0530

    Time: 6 ms (70.82%) | Memory: 45.1 MB (87.69%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,33 +1,32 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int ans = Integer.MAX_VALUE;
         int m = matrix.length;
         int n = matrix[0].length;
 
-        int[][]dp = new int[m][n];
-        for(int [] arr : dp){
-            Arrays.fill(arr , Integer.MAX_VALUE);
-        } 
-        for(int i =0 ; i<1;i++){
-            for(int j =0 ;j<n ;j++){
-                int val = helper(i , j , matrix , dp);
-                ans= Math.min(ans , val);
+        int [] prev = matrix[0].clone();
+       
+
+        for(int i = 1 ; i < m ; i++){
+             int [] curr =  new int[n];
+            for(int j = 0 ; j< n ; j++){
+                int up = prev[j];
+                int upleft =Integer.MAX_VALUE;
+                int upright =Integer.MAX_VALUE;
+
+                if(j > 0){
+                     upleft = prev[j-1];
+                }
+                if(j<n-1){
+                     upright = prev[j+1];
+                }
+                curr[j]= matrix[i][j] + Math.min(up, Math.min(upleft , upright));
             }
+            prev = curr;
+        }
+        int ans = Integer.MAX_VALUE;
+        for(int i =0; i< n ; i++){
+            ans = Math.min(ans , prev[i]);
         }
         return ans;
-    }
-    public static int helper(int i , int j , int[][]arr , int [][] dp){
-        if(i < 0 || j <0 || i>= arr.length || j >= arr[0].length)return Integer.MAX_VALUE;
-
-        if(i == arr.length-1)return arr[i][j];
-
-        if(dp[i][j] != Integer.MAX_VALUE)return dp[i][j];
-
-        int down = helper(i+1 , j , arr , dp);
-        int downLeft = helper(i+1 , j-1 , arr , dp);
-        int downRight = helper(i+1 , j+1 , arr , dp);
-
-        dp[i][j] = arr[i][j] + Math.min(down , Math.min(downLeft ,downRight ));
-        return dp[i][j];
     }
 }
