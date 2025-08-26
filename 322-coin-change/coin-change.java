bffd9c9e581ee978bb59920ed6a8bd01c0da1a79
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-08-26 10:52:29 +0530

    Time: 30 ms (20.44%) | Memory: 45.3 MB (21.61%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,33 +1,29 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int [][] dp = new int[n][amount+1];
-        for(int i =0 ; i< n ; i++){
-            Arrays.fill(dp[i] , -1);
-        }
-        int result= helper(n-1 , amount , coins , dp);
-        if (result == Integer.MAX_VALUE - 2) {
-        return -1;
-        }
-        return result;
-    }
-    public int helper(int ind , int target , int[] nums , int [][] dp){
-        if (target == 0) return 0;
         
-        if(ind == 0){
-          if(target % nums[ind] ==0){
-            return target / nums[ind];
-          }else{
-            return Integer.MAX_VALUE-2;
-          }
+        int [][] dp = new int[n][amount +1];
+        for(int[] i : dp){
+            Arrays.fill(i  , -1);
         }
+        int ans = helper(n-1 , amount , coins , dp);
+        if(ans == Integer.MAX_VALUE-500)return -1;
+        return ans;
+    }
+    public static int helper(int ind , int target , int [] nums , int [][] dp){
+        if(ind == 0){
+            if(target % nums[ind]== 0)return target/nums[ind];
+            return Integer.MAX_VALUE-500;
+        }
+
         if(dp[ind][target] != -1)return dp[ind][target];
 
-        int nonTake = helper(ind - 1 , target , nums , dp);
-        int take = Integer.MAX_VALUE-2;
-        if(target >= nums[ind]){
-            take = 1 + helper(ind  , target - nums[ind] , nums , dp); 
+        int nonTake = 0 + helper(ind -1, target , nums , dp);
+        int take = Integer.MAX_VALUE;
+        if(nums[ind] <= target){
+            take = Math.min(take , 1 + helper(ind ,target -  nums[ind] , nums , dp ));
         }
-        return dp[ind][target] = Math.min(take , nonTake);
+        dp[ind][target] = Math.min(take , nonTake);
+        return dp[ind][target];
     }
 }
