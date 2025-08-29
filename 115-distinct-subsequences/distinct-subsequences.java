5e423724db5e800fd34373582fd1db5bbf8cea39
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-08-29 12:14:06 +0530

    Time: 18 ms (79.02%) | Memory: 51.8 MB (13.38%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,27 +1,35 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int m = s.length();
-        int n = t.length();
-        int [][] dp = new int[m][n];
-        for(int [] arr : dp){
-            Arrays.fill(arr , -1);
+        // Create a 2D array to store the counts of subsequences
+        int prime = (int) (Math.pow(10, 9) + 7);
+        int n = s.length();
+        int m = t.length();
+        int dp[][] = new int[n + 1][m + 1];
+
+        // Initialize the first column with 1 because there's one empty subsequence in any string.
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = 1;
         }
-        return helper(0 , 0 , s , t, dp);
-    }
-    public int helper(int idx1 , int idx2 , String s , String t , int [][] dp){
-        if(idx2 == t.length())return 1;
 
-        if(idx1 == s.length())return 0;
-
-        if(dp[idx1][idx2] != -1)return dp[idx1][idx2];
-
-        int notTake = helper(idx1 +1 , idx2 , s , t, dp);
-        int take =0;
-        if(s.charAt(idx1) == t.charAt(idx2)){
-            take = helper(idx1+1 , idx2 +1,s ,t, dp);
+        // Initialize the first row (except dp[0][0]) with 0 because there's no way to form s2 from an empty string.
+        for (int i = 1; i < m + 1; i++) {
+            dp[0][i] = 0;
         }
-        dp[idx1][idx2] = take + notTake;
 
-        return dp[idx1][idx2];
+        // Fill the dp array using a bottom-up approach
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    // If the characters match, we can either include this character in the subsequence
+                    // or exclude it. So, we add the counts from both possibilities.
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) ;
+                } else {
+                    // If the characters don't match, we can only exclude this character.
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 }
