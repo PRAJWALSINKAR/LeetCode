ca0a1745fb7f4c3df9920bdcd972d07122008504
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-08-29 12:36:09 +0530

    Time: 18 ms (79.02%) | Memory: 51.4 MB (41.75%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,35 +1,25 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        // Create a 2D array to store the counts of subsequences
-        int prime = (int) (Math.pow(10, 9) + 7);
-        int n = s.length();
-        int m = t.length();
-        int dp[][] = new int[n + 1][m + 1];
+        int m = s.length();
+        int n = t.length();
+        int[][] dp = new int[m + 1][n + 1];
 
-        // Initialize the first column with 1 because there's one empty subsequence in any string.
-        for (int i = 0; i < n + 1; i++) {
+        for (int i = 0; i <= m; i++) {
             dp[i][0] = 1;
         }
 
-        // Initialize the first row (except dp[0][0]) with 0 because there's no way to form s2 from an empty string.
-        for (int i = 1; i < m + 1; i++) {
+        for (int i = 1; i <= n; i++) {
             dp[0][i] = 0;
         }
-
-        // Fill the dp array using a bottom-up approach
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < m + 1; j++) {
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
                 if (s.charAt(i - 1) == t.charAt(j - 1)) {
-                    // If the characters match, we can either include this character in the subsequence
-                    // or exclude it. So, we add the counts from both possibilities.
-                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) ;
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
                 } else {
-                    // If the characters don't match, we can only exclude this character.
                     dp[i][j] = dp[i - 1][j];
                 }
             }
         }
-
-        return dp[n][m];
+        return dp[m][n];
     }
 }
