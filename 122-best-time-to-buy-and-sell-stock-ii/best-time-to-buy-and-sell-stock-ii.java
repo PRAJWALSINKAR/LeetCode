86609529ebea9c8415fa15013fd21f4a8e8e32d4
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-08-29 22:48:29 +0530

    Time: 1 ms (77.35%) | Memory: 46.1 MB (27.29%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,24 +1,27 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int [][] dp = new int[n][2];
-        for(int arr[] : dp){
-            Arrays.fill(arr , -1);
+        int[][] dp = new int[n + 1][2]; 
+        // dp[n][0] = dp[n][1] = 0  (base case already default)
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                int profit = 0;
+                if (buy == 0) { // can buy
+                    profit = Math.max(
+                        -prices[ind] + dp[ind + 1][1], // buy
+                        dp[ind + 1][0]                 // skip
+                    );
+                } else { // can sell
+                    profit = Math.max(
+                        prices[ind] + dp[ind + 1][0],  // sell
+                        dp[ind + 1][1]                 // skip
+                    );
+                }
+                dp[ind][buy] = profit;
+            }
         }
-        return helper(0 , 0 , prices , dp  );
-    }
-    public int helper(int ind , int buy , int[] arr , int[][] dp ){
-        if(ind == arr.length )return 0;
 
-        if(dp[ind][buy] != -1)return dp[ind][buy];
-
-        int profit = 0;
-
-        if(buy==0){
-            profit = Math.max(- arr[ind] + helper(ind +1,1,arr,dp ),helper(ind+1,0,arr,dp));//Maath.max(byu , skip)
-        }else{
-            profit=Math.max(arr[ind] + helper(ind +1,0,arr,dp) , helper(ind+1 , 1,arr,dp));
-        }
-        return dp[ind][buy] = profit;
+        return dp[0][0]; // answer starting at day 0, in "can buy" state
     }
 }
