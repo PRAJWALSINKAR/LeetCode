37ea365c4c6f46050d94e93bcf6ab062b37fc223
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-08-30 10:52:48 +0530

    Time: 1 ms (77.30%) | Memory: 46.4 MB (14.79%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,27 +1,19 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int[][] dp = new int[n + 1][2]; 
-        // dp[n][0] = dp[n][1] = 0  (base case already default)
+    public int maxProfit(int[] arr) {
+        int n = arr.length;
+        int [][] dp = new int[n+1][2];
 
-        for (int ind = n - 1; ind >= 0; ind--) {
-            for (int buy = 0; buy <= 1; buy++) {
+        for(int ind= n-1 ; ind>=0 ; ind--){
+            for(int buy =0 ; buy <= 1;buy++){
                 int profit = 0;
-                if (buy == 0) { // can buy
-                    profit = Math.max(
-                        -prices[ind] + dp[ind + 1][1], // buy
-                        dp[ind + 1][0]                 // skip
-                    );
-                } else { // can sell
-                    profit = Math.max(
-                        prices[ind] + dp[ind + 1][0],  // sell
-                        dp[ind + 1][1]                 // skip
-                    );
+                if(buy == 0){
+                profit = Math.max(dp[ind+1][0] , -arr[ind]+dp[ind+1][1]);
+                }else{
+                    profit =Math.max(dp[ind+1][1], arr[ind]+dp[ind+1][0]);
                 }
                 dp[ind][buy] = profit;
             }
         }
-
-        return dp[0][0]; // answer starting at day 0, in "can buy" state
+        return dp[0][0];
     }
 }
