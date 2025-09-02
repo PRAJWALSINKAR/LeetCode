4e7f8a2c8577480a41b1ae9a19507187862616f6
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-09-02 18:20:13 +0530

    Time: 27 ms (17.66%) | Memory: 42.9 MB (26.56%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -1,34 +1,28 @@
 class Solution {
     public int minCost(int n, int[] cuts) {
-        int  len = cuts.length;
-
         ArrayList<Integer> lis = new ArrayList<>();
-
-        for(int z : cuts)lis.add(z);
+        int len = cuts.length;
+        for(int i : cuts)lis.add(i);
 
         lis.add(0);
         lis.add(n);
         Collections.sort(lis);
 
         int dp[][] = new int[len+2][len+2];
- 
-        for(int [] arr : dp){
-            Arrays.fill(arr , -1);
+
+        for(int [] ar : dp){
+            Arrays.fill(ar , -1);
         }
-        return helper(1 ,len , lis , dp );
+        return helper(1,len , lis , dp);
     }
-    public int helper(int i , int j ,ArrayList<Integer> lis , int[][]dp ){
+    public static int helper(int i , int j ,ArrayList<Integer> lis, int[][]dp ){
         if(i > j)return 0;
         if(dp[i][j] != -1)return dp[i][j];
         int min = Integer.MAX_VALUE;
-        for (int ind = i; ind <= j; ind++) {
-            int cost = lis.get(j + 1) - lis.get(i - 1) +
-                       helper(i, ind - 1, lis, dp) +
-                       helper(ind + 1, j, lis, dp);
-
-            min = Math.min(min, cost);
+        for(int ind=i ; ind<= j ;ind++){
+            int temp = lis.get(j+1)-lis.get(i-1)+helper(i, ind-1, lis, dp)+helper(ind+1, j , lis,dp);
+            min =Math.min(min,temp);
         }
-
         return dp[i][j] = min;
     }
 }
