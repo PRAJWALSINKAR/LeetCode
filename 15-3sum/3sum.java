5ef4139cb041344207e42a68759cd157caffd83f
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-09-09 22:01:12 +0530

    Time: 31 ms (56.24%) | Memory: 52.7 MB (18.27%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,29 +1,29 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> abs = new ArrayList<>();
-        Arrays.sort(nums);//-4,-1,-1,0,1,2
-        
-         for(int i =0; i <nums.length-2;i++){
-           if(i>0 && nums[i] ==nums[i-1]) continue;
-            int left = i+1;
-            int right = nums.length-1;
-            while(left< right){
-                int sum = nums[i]+ nums[left]  +nums[right];
-                if(sum == 0){
-                    abs.add(Arrays.asList(nums[i],nums[left],nums[right])); 
-                    left++;
-                    right--;
-                    while(left<right && nums[left] == nums[left-1]) left++;
-                    while(left<right && nums[right] == nums[right+1]) right--;
-                }
-                else if(sum < 0){
-                    left++;
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i = 0 ; i<n-2 ; i++){
+            if(i > 0 && nums[i]  == nums[i-1])continue;
+            int j = i+1;
+            int k = n-1;
+            while(j<k){
+                if(nums[i] + nums[j] + nums[k] == 0){
+                    List<Integer> lis = new ArrayList<>();
+                    lis.add(nums[i]);
+                    lis.add(nums[j]);
+                    lis.add(nums[k]);
+                    ans.add(lis);
+                    while(j < k && nums[j] == nums[j+1])j++;
+                    while(j < k && nums[k] == nums[k-1])k--;
+                 }
+                if(nums[j] + nums[k] + nums[i] > 0){
+                    k--;
                 }else{
-                    right--;
+                    j++;
                 }
             }
         }
-        System.gc();
-        return abs;
+        return ans;
     }
 }
