b797ba343e0af87df1f69a7dd6f7ca2f2f993f75
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-09-13 22:06:32 +0530

    Time: 7 ms (24.33%) | Memory: 42.5 MB (40.24%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,21 +1,22 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        StringBuilder sb = new StringBuilder();
-        int count = 0;
+        Stack<Character> stk = new Stack<>();
+        StringBuilder str = new StringBuilder();
+        
+        int start = 0; // To track start of primitive
+        
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
-                count++;
-                if (count > 1) {
-                    sb.append('(');
-                }
+                stk.push('(');
             } else {
-                if (count > 1) {
-                    sb.append(')');
+                stk.pop();
+                if (stk.isEmpty()) {
+                    // Add inner part (exclude outermost '(' and ')')
+                    str.append(s.substring(start + 1, i));
+                    start = i + 1; // update start for next primitive
                 }
-                 count--;
             }
         }
-        String sp = sb.toString();
-        return sp;
+        return str.toString();
     }
 }
