400ef79e02765f48c302522210a9a155802b855a
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-09-15 21:25:57 +0530

    Time: 5 ms (91.59%) | Memory: 42.8 MB (86.74%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,49 +1,44 @@
-//****focus on clean space method*****
 class Solution {
     public String reverseWords(String s) {
-        char[] a = s.toCharArray();
-        int n = a.length;
-        reverse(0, n-1, a);
-        WordReverse(n, a);
-        return cleanSpace(n, a);
+        int n =s.length();
+        char [] arr = s.toCharArray();
+        reverse(0,n-1,arr);
+        reverseWord(0 , n-1 , arr);
+        return clean(0 ,  arr);
+        
     }
-
-    public void reverse(int i, int j, char[] a) {
-        while (i < j) {
-            char temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
+    public void reverse(int i , int j, char [] arr){
+        while(i < j){
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j]= temp;
             i++;
             j--;
         }
     }
-
-    public void WordReverse(int n, char[] a) {
-        int low = 0;
-        int high = 0;
-        while (low < n) {
-            while (low < n && a[low] == ' ')low++;
-            high = low ;
-            while (high < n && a[high] != ' ')high++;
-            reverse(low, high - 1, a);
-            low = high;
+    public void reverseWord(int i , int j , char [] arr){
+        while(i < arr.length){
+            while(i <arr.length &&arr[i] ==' ')i++;
+            j=i;
+            while(j< arr.length && arr[j] !=' ')j++;
+            reverse(i , j-1 , arr);
+            i = j;
         }
     }
+    public String clean (int i  , char[] arr){
+        int j =0;
+        StringBuilder sb = new StringBuilder();
+        while( i < arr.length){
+            while(i<arr.length && arr[i] ==' ')i++;
+            j=i;
+            while(j < arr.length && arr[j] != ' ' )sb.append(arr[j++]);
+            i=j;
+            while(i< arr.length && arr[i] ==' '){
+                if(i<arr.length-1 && arr[i+1] !=' ')sb.append(' ');
+                i++;
+            }
 
-    public String cleanSpace(int n, char[] a) {
-        int i = 0, j = 0;
-        while (i < n) {
-            while (i < n && a[i] == ' ')i++;
-            while (i < n && a[i] != ' ') a[j++] = a[i++];
-            while (i < n && a[i] == ' ') i++;
-            if (i < n)
-                a[j++] = ' ';
         }
-        return new String(a).substring(0, j);
-    }
+        return sb.toString();
+    } 
 }
-
-/*
-time = O(N)
-space =O(N)
-*/
