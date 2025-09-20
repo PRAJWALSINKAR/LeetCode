ee2a53ea0337359d7bed20b582b4396f5cec3ea6
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-09-20 08:27:23 +0530

    Time: 3 ms (72.05%) | Memory: 43.3 MB (53.72%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,31 +1,21 @@
-//my approch
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates);
-        List<List<Integer>> res = new ArrayList<>();
-
-        helper(0, 0, res, new ArrayList<>(), target, candidates);
-        return res;
+    public List<List<Integer>> combinationSum2(int[] arr, int target) {
+        List<List<Integer>> lis = new ArrayList<>();
+        Arrays.sort(arr);
+        helper(0 , lis , arr ,new ArrayList<>(), target );
+        return lis;
     }
-
-    public void helper(int index, int currSum, List<List<Integer>> res, List<Integer> temp, int target,
-            int[] candidate) {
-        if (currSum == target) {
-            res.add(new ArrayList<>(temp));
-            return;
+    public void helper(int ind ,List<List<Integer>> lis , int [] arr,List<Integer> temp , int target ){
+        if(target == 0){
+          lis.add(new ArrayList<>(temp));
+          return;
         }
-        if (index == candidate.length || currSum > target)
-            return;
-
-        temp.add(candidate[index]);
-        helper(index+1, currSum + candidate[index], res, temp, target, candidate);
-        temp.remove(temp.size() - 1);
-
-        while (index < candidate.length - 1 && candidate[index] == candidate[index + 1]) {
-            index++;
-        }
-
-        helper(index + 1, currSum, res, temp, target, candidate);
-
+    for(int i = ind ; i < arr.length ;i++){
+        if(i > ind && arr[i] == arr[i-1])continue;
+        if(arr[ind] > target)break;
+        temp.add(arr[i]);
+        helper(i+1 , lis , arr , temp , target - arr[i]);
+        temp.remove(temp.size() -1);
+    }
     }
 }
