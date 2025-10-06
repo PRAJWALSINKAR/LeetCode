99848612ee7328ac25995fd9bcc6c54d583744c6
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-10-06 23:08:56 +0530

    Time: 11 ms (59.42%) | Memory: 44.9 MB (15.97%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.java
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -1,40 +1,29 @@
 class Solution {
     public int swimInWater(int[][] grid) {
-        int low = 0;
-        int high = 2500;
         int n = grid.length;
-        int m = grid[0].length;
+        int res = 0;
+        boolean [][] vis = new boolean[n][n];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0] );
+        pq.add(new int[] {grid[0][0] , 0 , 0});
+        vis[0][0]=true;
+        int [][] direction = {{0,1} , {1,0} , {-1 , 0 }, {0,-1}};
+        
+        while(!pq.isEmpty()){
+            int [] arr = pq.poll();
+            int row = arr[1];
+            int col = arr[2];
+            res = Math.max(res, arr[0]);
+            if(row == n-1 && col == n-1 )break;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            boolean[][] vis = new boolean[n][m];
-
-            if (isPossible(grid, mid, 0, 0, vis, n, m)) {
-                high = mid - 1; // try to find smaller possible time
-            } else {
-                low = mid + 1; // increase water level
+            for(int dir[] : direction){
+                int xx = row + dir[0];
+                int yy = col + dir[1];
+                if(xx >= 0 && yy >= 0 && xx < n && yy < n && vis[xx][yy]==false){
+                pq.add(new int[]{grid[xx][yy] , xx , yy});
+                vis[xx][yy] =true; 
+                }
             }
         }
-        return low; // minimum time when path is possible
-    }
-
-    public boolean isPossible(int[][] grid, int val, int row, int col, boolean[][] vis, int n, int m) {
-        // Out of bounds or too high elevation or already visited
-        if (row < 0 || row >= n || col < 0 || col >= m || grid[row][col] > val || vis[row][col]) 
-            return false;
-
-        // Reached destination
-        if (row == n - 1 && col == m - 1) 
-            return true;
-
-        vis[row][col] = true;
-
-        // Explore 4 directions
-        boolean up = isPossible(grid, val, row - 1, col, vis, n, m);
-        boolean down = isPossible(grid, val, row + 1, col, vis, n, m);
-        boolean left = isPossible(grid, val, row, col - 1, vis, n, m);
-        boolean right = isPossible(grid, val, row, col + 1, vis, n, m);
-
-        return up || down || left || right;
+        return res;
     }
 }
