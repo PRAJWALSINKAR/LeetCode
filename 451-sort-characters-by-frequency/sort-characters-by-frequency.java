f0fcd6eeb9d28a62e3394bd4d94814935ccb307f
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-10-09 17:36:04 +0530

    Time: 21 ms (16.46%) | Memory: 45.5 MB (81.24%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,33 +1,34 @@
 class Solution {
     public String frequencySort(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        int maxFreq = 0;
-
-        for (char ch : s.toCharArray()) {
-            int freq = map.getOrDefault(ch, 0) + 1;
-            map.put(ch, freq);
-            maxFreq = Math.max(maxFreq, freq);
-        }
-
-        
-        List<List<Character>> bucket = new ArrayList<>();
-
-        for (int i = 0; i <= maxFreq; i++) {
-            bucket.add(new ArrayList<>());
-        }
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            char ch = entry.getKey();
-            int freq = entry.getValue();
-            bucket.get(freq).add(ch);
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int i = maxFreq; i > 0; i--) {
-            for (char ch : bucket.get(i)) {
-                for (int j = 0; j < i; j++) {
-                    sb.append(ch);
-                }
+        HashMap<Character , Integer> map = new HashMap<>();
+        int max = 0;
+        for(int i= 0 ; i< s.length() ; i++){
+            char ch = s.charAt(i);
+            map.put(ch , map.getOrDefault(ch , 0) +1);
+            if(map.get(ch) > max){
+                max = map.get(ch);
             }
         }
+        List<List<Character>> lis = new ArrayList<>();
+
+        for(int i = 0 ; i <= max ; i++){
+            lis.add(new ArrayList<>());
+        }
+
+        for(Map.Entry<Character , Integer> entry : map.entrySet()){
+            char key = entry.getKey();
+            int val = entry.getValue();
+
+            lis.get(val).add(key);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = max ; i>=0 ; i--){
+            for(char ch : lis.get(i)){
+                for(int j = 0 ; j < i ; j++){
+                     sb.append(ch);
+                    }
+                }
+            }
         return sb.toString();
     }
 }
