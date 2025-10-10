c1fcc37e153977582242b4b924f80cad8b78ac0c
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-10-10 22:31:35 +0530

    Time: 2 ms (97.93%) | Memory: 61.5 MB (48.97%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.java
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.java
@@ -1,21 +1,22 @@
 class Solution {
     public int maximumEnergy(int[] arr, int k) {
         int n = arr.length;
-        int[] dp = new int[n];
+        int[] dp = new int[n];// to store a sums 
+
         for (int i = 0; i < n; i++) {
-            if (i < k) {
+            if (i < k) { // if previous steps not possible
                 dp[i] = arr[i];
-                continue;
+            } else {
+                dp[i] = Math.max(arr[i], arr[i] + dp[i - k]);// we take previous sum and current max
             }
-            dp[i] =Math.max(arr[i] ,  arr[i] + dp[i - k]);
         }
+        
         int ans = Integer.MIN_VALUE;
-        for (int j = n - 1; j >= 0; j--) {
-            if (k == 0)
-                break;
+        
+        for (int j = n - 1; j >= n-k; j--) { // loop for cheaking last value k values
             ans = Math.max(ans, dp[j]);
-            k--;
         }
+        
         return ans;
     }
 }
