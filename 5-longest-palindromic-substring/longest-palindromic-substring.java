0aafa5f0c222d0a3c4cde51fcc6f3057d6a1bdd5
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-10-12 20:20:38 +0530

    Time: 14 ms (92.62%) | Memory: 42.6 MB (56.23%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,35 +1,27 @@
 class Solution {
     public String longestPalindrome(String s) {
         int n = s.length();
-        if (s == null && n == 0)
-            return "";
-
-        int start = 0;
+        int start =0;
         int end = 0;
-        for (int i = 0; i < n; i++) {
-            int odd_Center = expand(s, i, i); // Odd length palindrome :-> aba
-            int even_Center = expand(s, i, i + 1);// Even length palindrome :-> abba
-            int max = Math.max(odd_Center, even_Center);
 
-            //for calculating new start and end points
-            if (max > end - start) {
-                start = i - (max - 1) / 2;
-                end = i + max / 2;
-            }
+        for(int i = 0 ; i< n ; i++){
+            int odd = helper(i , i , s);
+            int even = helper(i , i+1 , s);
+
+            int max = Math.max(odd , even);
+            if(max > end - start){
+              start = i - (max -1)/2;
+              end = max/2 +i ; 
+           }
         }
-        return s.substring(start, end + 1);
+        return s.substring(start , end+1);
+
     }
-
-    //here we just go left side and right side and camparing it 
-    public int expand(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
+    public int helper(int i , int j , String s ){
+        while(i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)){
+            i--;
+            j++;
         }
-        return right - left - 1;
+        return j -i-1;
     }
 }
-/*
-Time: O(n²) → Due to expansion for every character
-Space: O(1) → No extra space used
-*/
