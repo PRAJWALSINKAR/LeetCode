62cf356f6818575da368efcef4669ab16c57c31e
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-10-14 19:05:39 +0530

    Time: 100 ms (11.55%) | Memory: 45.4 MB (62.34%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.java
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -1,19 +1,11 @@
-//brute approch
 class Solution {
     public String shortestPalindrome(String s) {
         String rev = new StringBuilder(s).reverse().toString();
-
-        for(int i = 0 ;i < s.length() ; i++){
-            
+        for(int i = 0 ; i < s.length() ; i++){
             if(s.startsWith(rev.substring(i))){
-                return rev.substring(0 , i) + s;
+                return rev.substring(0, i) + s ;
             }
         }
-        return "";
+        return rev + s;
     }
 }
-
-/*
-Time Complexity: O(n²) – Due to startsWith() inside a loop.
-Space Complexity: O(n) – For the reversed string and result.
-*/
