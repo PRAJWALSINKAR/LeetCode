9cb0923d491e61d228c82d5a43587475b6f81012
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-11-03 21:38:13 +0530

    Time: 3 ms (99.73%) | Memory: 94.4 MB (5.17%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -8,38 +8,36 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
- // int privious sumbition when we use recursion it will take an extra space and extra time 
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        //first We found out an middle
-        if(head == null || head.next == null)return true;
-        ListNode slow  = head;
-        ListNode fast = head;
-
-        while(fast.next != null && fast.next.next != null){
+        ListNode slow = head;
+        if(slow.next == null)return true;
+        ListNode fast = head.next;
+        if(fast.next == null){
+            return slow.val == fast.val;
+        }
+        int count =1;
+        while(fast.next != null && fast.next.next != null ){
             slow = slow.next;
             fast = fast.next.next;
+            count++;
         }
-       //reverse  2nd half
-      ListNode newHead = reverse(slow.next);
-       ListNode first = head;
-       ListNode secound = newHead;
-       while(secound != null){
-        if(first.val != secound.val)return false;
-        first = first.next;
-        secound = secound.next;
-       }
-       return true;
-    }
-    private ListNode reverse(ListNode head){
-      ListNode curr = head;
-        ListNode prev = null;
+        ListNode dummy = null;
+        ListNode curr = slow.next;
+
         while(curr != null){
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+            ListNode prev = curr;
+            curr = curr.next;
+            prev.next = dummy;
+            dummy = prev;
         }
-        return prev;
+        curr = head;
+        while(count > 0){
+            if(curr.val != dummy.val)return false;
+            curr = curr.next;
+            dummy = dummy.next;
+            count--;
+        }
+        return true;
     }
 }
