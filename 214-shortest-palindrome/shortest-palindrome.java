64ed9c1cde50afc8bc6f927673b654d6c48424c1
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-11-10 20:29:14 +0530

    Time: 7 ms (88.40%) | Memory: 46.5 MB (6.25%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.java
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -1,11 +1,33 @@
 class Solution {
     public String shortestPalindrome(String s) {
-        String rev = new StringBuilder(s).reverse().toString();
-        for(int i = 0 ; i < s.length() ; i++){
-            if(s.startsWith(rev.substring(i))){
-                return rev.substring(0, i) + s ;
+        StringBuilder sb = new StringBuilder(s);
+        String rev = sb.reverse().toString();
+        String arr = s + '*' +  rev;
+        int x = s.length() - kmp(arr);
+        String start = rev.substring(0, x);
+        return start + s;
+    }
+
+    public int kmp(String rev){
+        int n = rev.length();
+        int [] arr = new int[n];
+
+        int i = 0;
+        int j = 1;
+
+        while(j < n){
+            if(rev.charAt(i) == rev.charAt(j)){
+                arr[j] = i+1;
+                i++;
+                j++;
+               
+            }else if(i > 0){
+               i = arr[i-1];
+            }else{
+                arr[j] = 0;
+                j++;
             }
         }
-        return rev + s;
+        return arr[n-1];
     }
 }
