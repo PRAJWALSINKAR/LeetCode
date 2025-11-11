28f39a3e650b640ee0b978727e5d3238274cc48c
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-11-12 00:18:01 +0530

    Time: 27 ms (72.17%) | Memory: 70 MB (7.04%) - LeetSync

--- 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.java
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.java
@@ -1,31 +1,23 @@
 class Solution {
     public int minimumSize(int[] nums, int k) {
-        int low= 1;//0
-        int high =Integer.MAX_VALUE;//7
-        int ans = 0;
-        // for(int i : nums){
-        //     high = Math.max(i , high);
-        // }
+        int low = 1;
+        int high = Integer.MAX_VALUE;
+        
+
         while(low <= high){
-            int mid = low + (high -low) / 2;
-            if(isPossible(mid , k , nums)){
-                high = mid-1;
-                ans = mid;
+            int mid = low + (high - low ) / 2 ;
+            int count = 0;
+            for(int i : nums){
+                count += (i-1) / mid;
+            }
+
+            if(count <= k){
+                high = mid -1;
             }
             else{
                 low = mid +1;
             }
         }
-        return ans ;
-    }
-    public boolean isPossible(int val , int k , int[] nums){//8
-        int count =0;
-        for(int i : nums){
-           if(i > val){
-             count += (i-1) / val;
-           }
-           if(count > k)return false;
-        }
-        return true;
+        return low;
     }
 }
