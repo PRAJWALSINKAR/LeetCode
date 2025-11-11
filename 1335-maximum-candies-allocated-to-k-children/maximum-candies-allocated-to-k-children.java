81ac2967a391eeba3e2dc7c9a0ac2131d9746e3c
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-11-11 23:50:21 +0530

    Time: 31 ms (38.08%) | Memory: 91.8 MB (10.93%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
@@ -1,28 +1,27 @@
 class Solution {
     public int maximumCandies(int[] candies, long k) {
-        long sum = 0;
-        for(int i : candies){
-            sum += i;
-        }
+       
         long low = 1;
-        long result= 0;
-        long high = sum /k;
-      
+        long high = 0;
+
+        for(int i : candies){
+            high = Math.max(i , high);
+        }
 
         while(low <= high){
+            long mid = low + (high-low) / 2;
             long count = 0;
-            long mid = (low + high) / 2;
-            
+
             for(int i : candies){
                 count += i/mid;
-            }
-            if(count >=k){
+            } 
+
+            if(count >= k){
                 low = mid+1;
-                result = mid;
             }else{
-                high = mid -1;
+                high = mid-1;
             }
-        } 
-        return (int)result;
+        }
+        return (int)high;
     }
 }
