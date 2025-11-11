4bd6e49671527043df8d66c28d1f8e8e009f1c37
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-11-11 19:39:20 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (6.90%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,23 +1,39 @@
 class Solution {
+    int low= 0 ;
     public int maximumCount(int[] nums) {
         int n = nums.length;
-        int neg= helper(nums , 0);
-        int pos =  n - helper(nums , 1);
+
+        int neg = helperneg(low , n-1 , nums)  ;
+        int pos = n  - helperpos(low,n-1 , nums );
         return Math.max(neg , pos);
     }
-    public int helper(int [] nums , int target){
-        int left = 0;
-        int right = nums.length-1;
-        int result = nums.length;
-        while(left<=right){
-        int mid = (left + right) / 2;
-            if(nums[mid] < target ){
-              left = mid + 1;
-            }else{
-                result = mid;
-                right  = mid-1;
+
+    public int helperneg(int low , int high , int [] nums){
+        while(low <= high){
+            int mid = low + (high - low)  / 2 ;
+            
+            if(nums[mid] < 0){
+                low = mid +1;
+            }
+            else{
+                high = mid -1;
             }
         }
-        return result;
+        return low;
     }
+
+    public int helperpos(int low , int high , int [] nums){
+        while(low <= high){
+            int mid = low + (high - low)  / 2 ;
+            
+            if(nums[mid] <=  0){
+                low = mid +1;
+            }
+            else{
+                high = mid -1;
+            }
+        }
+        return low;
+    }
+
 }
