e968a02fa4e9bd7b3386ab751a13bd176340abdd
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-11-12 20:36:41 +0530

    Time: 1 ms (99.88%) | Memory: 45.8 MB (8.01%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,22 +1,25 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-       List<List<Integer>> res = new ArrayList<>();
-       helper(0 , candidates , target , new ArrayList<>() ,res);
-       return res;
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(0 , 0,target, candidates, new ArrayList() , ans);
+        return ans;
     }
-    public void helper(int ind , int [] arr , int target , List<Integer> temp ,List<List<Integer>> res  ){
-        if(target == 0){
-            res.add(new ArrayList<>(temp));
+
+    public void helper(int ind , int curr , int target ,int [] arr ,  List<Integer> lis , List<List<Integer>> ans   ){
+        if(ind >= arr.length || curr > target)return;
+        if(target == curr){
+            ans.add(new ArrayList<>(lis));
             return;
         }
-        if(ind == arr.length)return;
-
-        if(target >= arr[ind]){
-            temp.add(arr[ind]);
-            helper(ind , arr , target - arr[ind] , temp , res);
-            temp.remove(temp.size() -1);
+        
+        if(arr[ind]+curr <= target){
+            lis.add(arr[ind]);
+            helper(ind , curr + arr[ind] , target , arr , lis , ans);
+              lis.remove(lis.size() -1 );
         }
-        helper(ind+1 , arr , target , temp , res);
+        
+      
 
+        helper(ind+1 , curr, target , arr , lis , ans);
     }
 }
