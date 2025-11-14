9f191b4c12dc238641edf059c24ca7ab482a60a5
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-11-14 20:20:08 +0530

    Time: 147 ms (46.21%) | Memory: 42.9 MB (31.99%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -2,37 +2,36 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         int m = board.length;
         int n = board[0].length;
-         boolean[][] vis = new boolean[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (word.charAt(0) == board[i][j]) {
-                    if(helper(0, i, j, board, word,vis))return true;
+        boolean [][] vis = new boolean[m][n];
+        for(int i = 0 ; i< m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(word.charAt(0) == board[i][j]){
+                    if(helper(0, i , j , vis , board , word))return true;
                 }
             }
         }
-        return false;
+        return false; 
     }
-    public boolean helper(int ind, int i, int j, char[][] board, String word , boolean[][]vis) {
-        if (ind == word.length())
-            return true;
+    public boolean helper(int ind ,int row , int col , boolean [][] vis , char[][] board , String s){
+        if(ind == s.length())return true;
 
-        if (i < 0 || j < 0 ||i > board.length - 1 || j > board[0].length - 1)
-            return false;
+        if(row < 0 || col < 0 || row >= vis.length || col >= vis[0].length)return false;
 
-        if (word.charAt(ind) != board[i][j] || vis[i][j] == true)
-            return false;
-        vis[i][j] = true;
-        //up
-        boolean up = helper(ind + 1, i - 1, j, board, word,vis);
-        //left
-        boolean left = helper(ind + 1, i, j - 1, board, word,vis);
-        // down
-        boolean down = helper(ind + 1, i + 1, j, board, word,vis);
-        //right
-        boolean right = helper(ind + 1, i, j + 1, board, word,vis);
+        if(vis[row][col] == true ||  board[row][col] != s.charAt(ind))return false;
 
-        vis[i][j] = false;
-        
-        return up || left || down || right; 
+        vis[row][col] =true;
+
+        boolean up = helper(ind+ 1 , row -1, col , vis , board , s );
+
+        boolean down = helper(ind+ 1 , row +1 , col , vis , board , s );
+
+        boolean left = helper(ind+ 1 , row , col-1 , vis , board , s );
+
+        boolean right = helper(ind+ 1 , row , col + 1 , vis , board , s );
+
+        vis[row][col] = false;
+        return up || down || left || right;
+
+
     }
 }
