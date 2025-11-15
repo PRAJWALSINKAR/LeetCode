881bb1a9aa23356d2980441c4ea699b17be3083f
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-11-15 19:35:17 +0530

    Time: 5 ms (27.09%) | Memory: 46.7 MB (8.36%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,46 +1,36 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> ans = new ArrayList<>();
+        boolean [] RL = new boolean[n*2+1];
+        boolean [] LR = new boolean[n*2+1];
+        boolean CL [] = new boolean[n+1]; 
         int [][] arr = new int[n][n];
-        helper(0 , arr , ans);
+        helper(0 , ans , arr ,  RL , LR , CL , n );
         return ans;
     }
-    public void helper(int row , int [][] arr , List<List<String>> ans ){
-        if(row == arr.length){
+    public void helper(int row , List<List<String>> ans , int [][] arr ,  boolean [] RL , boolean [] LR, boolean [] CL , int n  ){
+        if(row == n){
             ans.add(new ArrayList<>(change(arr)));
             return;
         }
         for(int col = 0 ; col < arr.length ; col++){
-            if(check(row , col , arr)){
+            if(check(row , col ,  RL , LR , CL , n )){
+                LR[row + col] = true;
+                RL[n+ (col - row)] = true;
+                CL[col] = true;
                 arr[row][col] = 1;
-                helper(row + 1 , arr , ans);
-                arr[row][col] = 0;
+                helper(row + 1 ,  ans , arr , RL , LR , CL , n);
+
+                arr[row][col] =0;
+                LR[row + col] = false;
+                RL[n+ (col - row)] = false;
+                CL[col] = false;
+
             }
         }
     }
-    public boolean check(int row , int col , int [][] arr){
-        int drow = row;
-        int dcol = col;
-        //left
-        while(row >= 0){
-            if(arr[row][col] == 1)return false;
-            row--;
-        }
-        //up-left
-        row = drow;
-        while(row >= 0  && col >=0){
-            if(arr[row][col] == 1 )return false;
-            row--;
-            col--;
-        }
-        row = drow;
-        col = dcol;
-        //up-right
-        while(row >= 0 && col < arr.length){
-            if(arr[row][col] == 1 )return false;
-            row--;
-            col++;
-        }
+    public boolean check(int row , int col , boolean [] RL , boolean [] LR, boolean [] CL , int n  ){
+        if(LR[row + col] == true || RL[n+ (col - row)] == true || CL[col] == true)return false;
         return true;
     }
     public List<String> change(int[][] arr){
