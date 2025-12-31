fd0d347a051bdc81164d0a6b60dbb2b539fcd7b1
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2025-12-31 19:08:39 +0530

    Time: 24 ms (78.91%) | Memory: 46.8 MB (22.26%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -2,39 +2,29 @@ class Solution {
     public int cherryPickup(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int [][][] dp= new int[n][m][m];
-        for(int [][] arr : dp){
-            for(int [] zp : arr){
-                Arrays.fill(zp , Integer.MIN_VALUE);
-            }
-        }
-        return helper(0,0,m-1,n , m , grid , dp);
+        Integer [][][] dp = new Integer[n][m][m];
+        return helper(0 , 0 , m-1,  grid , dp);
     }
+    public int helper(int row , int col1 , int col2 , int [][]grid, Integer[][][] dp ){
+        if(row == grid.length)return 0;
 
-    public int helper(int row , int col1,int col2 , int rowl, int coll,int [][]arr , int[][][]dp){
-        if(row <0 || col1<0 || col2<0 || row >= rowl || col1 >=coll || col2 >= coll)return Integer.MIN_VALUE;
-
-        if(row == rowl - 1){
-            if(col1 == col2){
-                return arr[row][col1];
-            }else{
-                return arr[row][col1] + arr[row][col2];
-            }
+        if(col1 < 0 || col2 < 0 || col1 >= grid[0].length || col2 >= grid[0].length)return 0;
+        
+        if(dp[row][col1][col2] != null)return dp[row][col1][col2];
+        int ans = 0;
+        if(col1 == col2){
+            ans += grid[row][col1]; 
+        }else{
+            ans += (grid[row][col1] + grid[row][col2]);
         }
-        if(dp[row][col1][col2] != Integer.MIN_VALUE)return dp[row][col1][col2];
-
         int max = Integer.MIN_VALUE;
-
-        for(int i =-1 ; i<2;i++){
-            for(int j = -1 ;j <2 ; j++){
-                if(col1 == col2){
-                max = Math.max(max ,arr[row][col1] + helper(row+1,col1 + i,col2+j,rowl,coll,arr,dp));
-                }else{
-                max =Math.max(max, arr[row][col1] +arr[row][col2] + helper(row+1,col1 + i,col2+j,rowl,coll,arr,dp));
-                }
+      
+        for(int i = -1 ; i < 2 ; i++){
+            for(int j =-1 ; j <2 ; j++){
+            
+                max = Math.max(max , helper(row+1 , col1 + i , col2 + j , grid , dp));
             }
         }
-        dp[row][col1][col2] = max;
-        return max;
+        return dp[row][col1][col2] =max+ans;
     }
 }
