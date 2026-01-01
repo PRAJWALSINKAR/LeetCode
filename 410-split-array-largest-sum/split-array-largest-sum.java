9cbde3b68334d97f0c8ec90d176879df74f48038
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2026-01-01 20:42:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (91.97%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -2,33 +2,35 @@ class Solution {
     public int splitArray(int[] nums, int k) {
         int low = 0;
         int high = 0;
-        for (int i : nums) {
-            low = Math.max(low, i);
-            high += i;
+
+        for(int i : nums){
+            high += i ;
+            low = Math.max(low, i); 
         }
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (split(nums, k, mid)) {
-                high = mid - 1;
-            } else {
+        
+        while(low <= high){
+            int mid = low + (high - low) /2 ;
+
+            if(helper(mid , nums , k)){
                 low = mid + 1;
+            }else{
+                high = mid-1;
             }
         }
         return low;
     }
+    public boolean helper(int day , int [] nums , int need ){
+        int sum = 0;
+        int count =1;
 
-    public boolean split(int [] nums , int target , int max){
-            int currSum = 0;
-            int split = 1 ;
-            for(int i : nums){
-                if(currSum + i  > max){
-                    split++;
-                    currSum = i;
-                }else{
-                    currSum += i;
-                }
-                if(split > target)return false;
+        for(int i : nums){
+            if(i+sum > day){
+                sum = i;
+                count++;
+            }else{
+                sum +=i;
             }
-            return true;
         }
+        return count > need;
+    }
 }
