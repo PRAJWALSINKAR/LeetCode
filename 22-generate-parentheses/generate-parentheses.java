a5ac190f1dfb490bc50028feb88039eb346e7c1a
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2026-01-01 22:36:58 +0530

    Time: 2 ms (69.07%) | Memory: 44.6 MB (55.45%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,18 +1,17 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> lis = new ArrayList<>();
-        helper("" ,  0,0, n , lis);
+        List<String> lis  = new ArrayList<>();
+        helper(0 ,0 ,n*2 ,lis , "");
         return lis;
     }
-    public void helper(String s , int op ,int cl ,  int n , List<String> lis){
-        if(op == n && cl == n){
+    public void helper(int op , int cl , int len , List<String> lis , String s){
+        if(s.length() == len ){
             lis.add(s);
             return;
         }
-        if(op < n){
-            helper(s + '(' , op+1 ,cl, n , lis);
-        }
-        if(cl < op)
-        helper(s+')' , op , cl+1 ,  n , lis);
+        if(op < len/2)
+        helper(op +1 ,cl, len , lis , s + "(");
+        if(op > cl)
+        helper(op ,cl+1 ,len , lis , s+")");
     }
 }
