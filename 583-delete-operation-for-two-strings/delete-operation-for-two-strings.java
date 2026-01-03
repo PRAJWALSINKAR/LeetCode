cf854b7feeb48ac7860a61eb357b233438ccc0b5
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2026-01-03 23:41:14 +0530

    Time: 7 ms (69.84%) | Memory: 46.4 MB (68.49%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,19 +1,21 @@
 class Solution {
-    public int minDistance(String s1, String s2) {
-        int n = s1.length();
-        int m = s2.length();
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
 
-        int[][] dp = new int[n+1][m+1];
+        Integer [][] dp = new Integer[n][m];
 
-        for(int i = 1 ; i<= n ;i++){
-            for(int j =1 ; j<= m ; j++){
-                if(s1.charAt(i-1) ==s2.charAt(j-1)){
-                   dp[i][j] = 1 + dp[i-1][j-1];
-                }else{
-                    dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
-                }
-            }
-        }
-        return n + m - 2*(dp[n][m]);
+        return helper(n-1 , m-1 , word1 , word2 , dp);
+    }
+    public int helper(int i , int j , String s1 , String s2 , Integer [][] dp){
+        if(i < 0 )return j +1;
+        if(j < 0 )return i +1;
+
+
+        if(dp[i][j] != null)return dp[i][j];
+
+        if(s1.charAt(i) == s2.charAt(j))return dp[i][j] =helper(i - 1 , j-1 , s1 , s2 , dp);
+
+        return dp[i][j] = 1 + Math.min(helper(i-1 , j , s1 , s2 , dp), helper(i ,j-1 , s1 , s2 , dp));
     }
 }
