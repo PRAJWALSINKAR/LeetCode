24ca06391d52f54d8bad6957a79429b4828fdf45
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2026-01-03 12:30:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (91.69%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -10,35 +10,40 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        if(head == null ||  head.next == null)return head;
-        
-        ListNode dummyHead = new ListNode(0);
-        dummyHead.next = head;
-    
-        ListNode L = dummyHead;
-        for(int i = 1 ; i < left ; i++){
-            L = L.next;
-        }
-
-        ListNode R = dummyHead;
-        for(int i = 0 ; i <= right ; i++){
-            R = R.next;
-        }
-        ListNode dummy = R;
-        
-        ListNode curr = L.next;
-        int count = right - left+1;
-
-        while(count > 0){
-
-            ListNode temp = curr;
+        if(head.next == null)return head;
+        if(right == left)return head;
+        ListNode curr = head;
+        ListNode  tail = null;
+        ListNode  rtail = null;
+        ListNode  rhead = null;
+        ListNode prev = null;
+        int count = 1;
+        while(curr != null){
+            if(count == left - 1)prev = curr;
+            if(count == left)rhead = curr;
+            if(count == right )rtail = curr;
+            if(count == right+1)tail = curr;
+            count++;
             curr = curr.next;
-            temp.next = dummy;
-            dummy = temp;
-            count--;
         }
-        L.next = dummy;
+        if(prev != null)
+        prev.next = null;
+        rtail.next = null;
+        reverse(rhead);
 
-        return dummyHead.next;
+        if(prev != null)
+        prev.next = rtail;
+        rhead.next = tail;
+        if(rhead == head)return rtail;
+        return head;
+    }
+    public void reverse(ListNode root){
+        ListNode dummy = null;
+        while(root != null){
+        ListNode temp = root;
+        root = root.next;
+        temp.next = dummy;
+        dummy = temp;
+        }
     }
 }
