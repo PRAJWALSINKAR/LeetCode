b6157bb35dfdeee6a84aa253c7e47d4eeaacebe1
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2026-01-24 23:52:21 +0530

    Time: 17 ms (23.99%) | Memory: 47.8 MB (18.19%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
@@ -1,23 +1,37 @@
 class Solution {
-    public int countCollisions(String s) {
-        int i = 0;
-        for( ; i<s.length() ; i++ ){
-            if(s.charAt(i) != 'L')break;
-        }
+    public int countCollisions(String dir) {
         int ans = 0;
-        int prev =0;
-        for(; i<s.length() ; i++){
-            char ch =s.charAt(i);
-            if(ch == 'S'){
-                ans += prev;
-                prev = 0;
-            }else if(ch == 'L'){
-                ans += (prev +1);
-                prev = 0;
-            }else{
-                prev++;
+        int left = 0;
+        boolean sti = false;
+        int right = 0;
+        int count = 0;
+        int n = dir.length();
+        for (int i = 0; i < n; i++) {
+            char ch = dir.charAt(i);
+
+            if (ch == 'R') {
+                right++;
+            } else if (ch == 'L') {
+                if (right != 0) {
+                    ans += 2;
+                    right--;
+                    sti = true;
+                    count = right;
+                    continue;
+                }
+                if (sti) {
+                    ans++;
+                }
+            } else {
+                sti = true;
+                if (right != 0) {
+                    ans += right;
+                    right = 0;
+                    count =0;
+                }
             }
         }
-        return ans;
+        
+        return ans + count;
     }
 }
