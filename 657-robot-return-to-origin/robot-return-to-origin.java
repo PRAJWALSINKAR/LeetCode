b95d150eeb0edf3d2b7880f68a375340a0ca8399
Prajwal Sinkar <sinkarprajwal2003@gmail.com>
2026-04-06 23:39:27 +0530

    Time: 7 ms (30.78%) | Memory: 45.7 MB (65.51%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -3,11 +3,13 @@ class Solution {
        int x = 0 , y = 0;
        for(int i = 0 ; i< s.length() ; i++){
         char ch = s.charAt(i);
-        if(ch == 'U')x--;
-        if(ch == 'D')x++;
-        if(ch == 'L')y--;
-        if(ch == 'R')y++;
+        if(ch == 'U')y++;  // +y axis
+        if(ch == 'D')y--;  // -y axis
+        if(ch == 'L')x--;  // -x axis
+        if(ch == 'R')x++;  //  +x axis 
        }
        return (x == 0 && y==0);
+
+       
     }
 }
